69059bb408f82fb0063daf572e43f114fd3f8c97
John Douratsos <dougian2@gmail.com>
2016-03-31 00:48:07 +0100

    Initial commit

---
+++ hackerrank/HelperFunctions.fs
@@ -0,0 +1,22 @@
+﻿namespace HelperFunctions
+
+open System
+
+module General = 
+    let read_lines = 
+        Seq.initInfinite (fun _ -> Console.ReadLine())
+        |>  Seq.takeWhile (fun x -> x<> null && x <> "")
+
+    let printSeq arr = 
+        let smart_print = printfn "%A"
+        arr |>
+            Seq.map smart_print
+
+    let rec my_filter pred arr = 
+        match arr with
+        | [] -> []
+        | head :: tail ->
+            if pred(head) then
+                List.append [head] (my_filter pred tail)
+            else
+                my_filter pred tail
---
+++ hackerrank/Program.fs
@@ -0,0 +1,54 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open HelperFunctions.General
+open System
+open System.Linq 
+
+(* problem with the list repeat *)
+
+let repeater repeat_times k =
+    Enumerable.Repeat (k,repeat_times)
+
+let list_replication argv = 
+    let repeat_times = 
+        Console.ReadLine()
+        |> Int32.Parse
+    let repeater_by_input = repeater repeat_times
+    let list_input = 
+        read_lines
+        |> Seq.map Int32.Parse
+        |> Seq.map repeater_by_input
+        |> Seq.concat
+        |> printSeq
+        |> Seq.iter ignore
+    0
+
+(* array filter problem *)
+
+let array_filter argv =
+    let delimiter = 
+        Console.ReadLine()
+        |> Int32.Parse
+    let data = 
+        read_lines
+        |> Seq.map Int32.Parse
+        |> List.ofSeq
+        |> my_filter (fun x -> x > delimiter)
+        |> printSeq
+        |> Seq.iter ignore
+    0
+    
+ (* filter positions *)
+[<EntryPoint>]
+let filter_positions argv =
+    let data = 
+        read_lines
+        |> Seq.map Int32.Parse
+        |> Seq.mapi (fun i k -> (i,k))
+        |> Seq.filter (fun t -> (fst t)%2 <> 0)
+        |> Seq.map snd
+        |> printSeq
+        |> Seq.iter ignore
+    0
+
